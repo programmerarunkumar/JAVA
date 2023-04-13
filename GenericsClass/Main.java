d75be114076b0525f8209168a2395346829359a8
arunkumar.mp <arunkumar.mp@zohocorp.com>
2023-04-13 21:23:33 +0530

    GenericsClass basic impl

---
+++ GenericsClass/Main.java
@@ -0,0 +1,15 @@
+package GenericsClass;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Pair<Integer, String> idName = new Pair<Integer, String>(455, "ARUNKUMAR.MP");
+        System.out.println("Key : " + idName.getKey() + " Value : " + idName.getValue());
+
+        Pair<String, Integer> nameAge = new Pair<String, Integer>("ARUNKUMAR.MP", 24);
+        System.out.println("Key : " + nameAge.getKey() + " Value : " + nameAge.getValue());
+
+    }
+
+}
---
+++ GenericsClass/Pair.java
@@ -0,0 +1,21 @@
+package GenericsClass;
+
+public class Pair<keyType, valueType> {
+
+    private keyType key;
+    private valueType value;
+
+    Pair(keyType key, valueType value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public keyType getKey(){
+        return this.key;
+    }
+
+    public valueType getValue(){
+        return this.value;
+    }
+
+}
