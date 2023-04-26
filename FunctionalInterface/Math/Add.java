46aad7792c2b2318530b21f72e1ef44b49ef2f14
programmerarunkumar <programmerarunkumar@gmail.com>
2023-04-26 12:35:26 +0530

    Functional Interface and Lambda Expression used for Math Operation (Add). Removed LamdaExpression Code

---
+++ FunctionalInterface/Math/Add.java
@@ -0,0 +1,8 @@
+package FunctionalInterface.Math;
+
+@FunctionalInterface
+public interface Add {
+
+    int perform(int a, int b);
+
+}
---
+++ FunctionalInterface/Math/Main.java
@@ -0,0 +1,16 @@
+package FunctionalInterface.Math;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        int a =1;
+        int b = 2;
+        Add add = (x, y) -> {
+            return x + y;
+        };
+        System.out.println(add.perform(a, b));
+
+    }
+    
+}
